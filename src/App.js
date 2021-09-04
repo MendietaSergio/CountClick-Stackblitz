723b79ffe85c7141171808adfb0982f78baf7207
MendietaSergio <mendieta.sergio.1995@gmail.com>
2021-09-04 11:25:40 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+import "./style.css";
+
+export default function App() {
+  const [ count, setCount ] = useState(0);
+  const [ time, setTime ] = useState( new Date().toLocaleTimeString())
+  const Contador =() =>{
+    setCount(count +1);
+    setTime(new Date().toLocaleTimeString())
+  }
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Contador de click: {count}</p>
+      <button onClick={()=>Contador()}>Click</button>
+      <p>Hora: {time}</p>
+    </div>
+  );
+}
